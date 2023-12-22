b5b193b05ec739cbf33a5e23c6ab312a9819f741
hanh <hanh@gmail.com>
2023-12-22 17:55:53 +0700

    a

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
-  res.render('a')
+  res.render('index')
 });
 
 app.get("/images", (req, res) => {
